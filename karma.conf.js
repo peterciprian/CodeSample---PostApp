273ec21669ac164743ba4967b20f3cf3b809fe85
OPC <peterciprian@gmail.com>
2021-01-19 10:24:10 +0100

    rename

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/bitNinja-PostApp'),
+      dir: require('path').join(__dirname, './coverage/PostApp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
